55b6fec2b1ef4b2c557f71cb8cfd9762ba196f3e
Abhishek <akg002@yahoo.in>
2016-05-14 18:21:54 +0530

    initial commit

---
+++ app/src/main/java/in/silive/directme/Dashboard.java
@@ -0,0 +1,19 @@
+package in.silive.directme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    void park(View V){
+        startActivity(new Intent(this,Map.class));
+    }
+}
---
+++ app/src/main/java/in/silive/directme/LoginActivity.java
@@ -0,0 +1,144 @@
+package in.silive.directme;
+
+import android.content.Intent;
+import android.support.v4.util.ArrayMap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener ,View.OnClickListener{
+    LoginButton fbLoginBtn;
+    CallbackManager fbCallBackManager ;
+    GoogleApiClient mGoogleApiClient ;
+    SignInButton googleSignInBtn ;
+    final int RC_SIGN_IN = 9231;
+    static final String TAG = "DirectMe";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplication());
+        setContentView(R.layout.activity_login);
+       setUpFbLogin();
+        setUpGoogleLogin();
+    }
+
+    //Google Sign In
+    void setUpGoogleLogin(){
+        googleSignInBtn = (SignInButton)findViewById(R.id.google_sign_in_btn);
+        googleSignInBtn.setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* Activity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Toast.makeText(this,"Sign in success"+acct.getDisplayName(),Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this,"Sign in failed",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Facebook LogIN
+    void setUpFbLogin(){
+        AppEventsLogger.activateApp(getApplication());
+        fbCallBackManager = CallbackManager.Factory.create();
+        fbLoginBtn = (LoginButton)findViewById(R.id.facebook_login_btn);
+        fbLoginBtn.setReadPermissions(Arrays.asList("public_profile","email"));
+        fbLoginBtn.registerCallback(fbCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken token = loginResult.getAccessToken();
+                //Getting Profile Info using token
+                GraphRequest request = GraphRequest.newMeRequest(
+                        token,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                try {
+                                    String name = object.getString("name");
+                                    Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        fbCallBackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.google_sign_in_btn :
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+                break;
+        }
+    }
+}
